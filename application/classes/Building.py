cb966cb68d3af9688b3a7a0574367390c788d756
Vladimir <keltecc@ya.ru>
2019-05-18 18:16:04 +0500

    change gitignore

--- application/classes/Building.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Building:
-    building_name: str
-    is_avangard: bool
-    info: str
-    image_filename: str
--- application/make_database.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists buildings;
-create table buildings (id integer primary key autoincrement, building_name varchar, isAvangard boolean, info varchar, image_filename varchar);
-insert into buildings (building_name, isAvangard, info, image_filename) values ("sdfsd", 0, "jhdfkj hjhjk hkjhjkh sdhj!", "sdfsd.png");
-insert into buildings (building_name, isAvangard, info, image_filename) values ("avangard", 1, "jhdj!", "avandard.png");
-insert into buildings (building_name, isAvangard, info, image_filename) values ("duck", 0, "krya-krya!", "duck.png");
-insert into buildings(building_name, isAvangard, info, image_filename) values ("jj", 0, "jj!", "jj.png");
-insert into buildings (building_name, isAvangard, info, image_filename) values ("sdddd", 0, "jhdddjhjkddkh sdhj!", "sddd.png");
