c36f5a65c3311ce846f3b8d5774f8ca62c34b015
amoniaka-knabino <cat1dog@yandex.ru>
2019-05-18 15:37:37 +0600

    class Building is added

--- application/classes/Game.py
+++ application/classes/Game.py
@@ -1,3 +1,13 @@
+import dataclasses
+
 class Game:
     def __init__(self):
         self.buildings = []
+
+@dataclasses.dataclass
+class Building:
+    building_name: str
+    is_avangard: bool
+    info: str
+    image_filename: str
+
