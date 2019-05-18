3f50344c1befecef03fc6760ac5a66cdaa8f71d5
amoniaka-knabino <cat1dog@yandex.ru>
2019-05-18 15:23:59 +0600

    initial commit

---
+++ application/classes/GameCreator.py
@@ -0,0 +1,10 @@
+import random
+
+class GameCreator(object):
+    def create_game(self):
+        pass
+
+
+class RandomGameCreator(GameCreator):
+    def create_game(self, n):
+        pass
---
+++ application/make_database.sql
@@ -0,0 +1,7 @@
+drop table if exists buildings;
+create table test(building_name varchar, isAvangard boolean, info varchar, image_filename varchar);
+insert into test (building_name, isAvangard, info, image_filename) values ("sdfsd", 0, "jhdfkj hjhjk hkjhjkh sdhj!", "sdfsd.png");
+insert into test (building_name, isAvangard, info, image_filename) values ("avangard", 1, "jhdj!", "avandard.png");
+insert into test (building_name, isAvangard, info, image_filename) values ("duck", 0, "krya-krya!", "duck.png");
+insert into test (building_name, isAvangard, info, image_filename) values ("jj", 0, "jj!", "jj.png");
+insert into test (building_name, isAvangard, info, image_filename) values ("sdddd", 0, "jhdddjhjkddkh sdhj!", "sddd.png");
